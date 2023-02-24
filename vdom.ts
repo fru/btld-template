578e61a7cb50f4e9925fa6de32b9dac3291b511a
Florian Rüberg <florian.rueberg@gmail.com>
2023-02-24 21:22:42 +0100

    Vdom api

--- vdom.ts
+++ vdom.ts
@@ -25,82 +25,72 @@ export class Vdom {
     state: {[key: string]: unknown};
     stateListener: {[key: string]: StateListener[]};
 
-    api = vdomApi(this);
-};
-
-type DomListener = (...args: any[]) => void;
-type StateListener = (before: unknown, after: unknown) => void;
-type VdomRoot = { vdom: Vdom };
-type ContentExp = string | { path: string[], listener: StateListener };
-type InnerHTML = string;
-
-function vdomApi(that: Vdom) {
-    return {
-        get tag() { return that.tag; },
-        setTag(tag: string) {
+    api = {
+        getTag: () => { return this.tag; },
+        setTag: (tag: string) => {
             // maybe dont allow this on root?
-            if (tag !== that.tag) {
-                that.tag = tag;
+            if (tag !== this.tag) {
+                this.tag = tag;
                 // create new element etc. remove old
                 // reapply listeners
                 // move children + attach
             }
         },
-        addDomListener() {},
+        addDomListener: () => {},
 
-        get root() { return that.root.vdom.api; },
-        get parent() { return that.parent.api; },
-        get children() { return that.children.map(c => c.api); },
+        getRoot: () => { return this.root.vdom.api; },
+        getParent: () => { return this.parent.api; },
+        getChildren: () => { return this.children.map(c => c.api); },
 
-        get detached() { return that.detached[that.mixin]; },
-        set detached(d) { that.detached[that.mixin] = d; },
-        setDetachedIfNotFirst(key: string) {
-            throw 'Not implemented exception - do else later';
+        isDetachedByMixin: () => { return this.detached[this.mixin]; },
+        setDetachedByMixin: (d: boolean) => { this.detached[this.mixin] = d; },
+        setDetachedIfNotFirst: (_key: string) => {
+            throw 'Not implemented exception - do $else later';
         },
-        isAnyDetached() {
+        isDetached: () => {
             // Check detached if not first
-            for (let mixin in that.detached) {
-                if (that.detached[mixin]) return true;
+            for (let mixin in this.detached) {
+                if (this.detached[mixin]) return true;
             }
         },
         
 
-        setState(key: string, value: unknown) {
+        setState: (key: string, value: unknown) => {
             // Get previous + Update state
             // Trigger Listeners
             // Propagate down unless it was set down locally
         },
-        setProperty(key: string, value: unknown) {
-            that.dom && (that.dom[key] = value);
+        setProperty: (key: string, value: unknown) => {
+            this.dom && (this.dom[key] = value);
         },
-        addStateListener(key: string, listener: StateListener) {
-            that.stateListener[key] = that.stateListener[key] || [];
-            that.stateListener[key].push(listener);
+        addStateListener: (key: string, listener: StateListener) => {
+            this.stateListener[key] = this.stateListener[key] || [];
+            this.stateListener[key].push(listener);
         },
 
-        setAttribute(name: string, value: ContentExp[]) {
+        setAttribute: (name: string, value: ContentExp[]) => {
             // detach all old listeners
-            that.attrs[name] = value;
+            this.attrs[name] = value;
             // set atttribute on dom
             // reattach listeners
         },
         
-        wrap(from: number, to: number) {
+        wrap: (from: number, to: number) => {
             // return new wrapping vdom api
         },
-        move(index: number, to: number) {
+        move: (index: number, to: number) => {
 
         },
-        append(nodes?: Definition) {
+        append: (nodes?: Definition) => {
             // Allows empty append, return singular appended vdom api
         },
 
-        setContent(content: ContentExp[] | InnerHTML) {
-            if (that.children.length) return; // cant override children
+        setContent: (content: ContentExp[] | InnerHTML) => {
+            if (this.children.length) return; // cant override children
             // mainly used by i18n
             // maybe split into two functions: setContent Exp | HTML
             // detach all old listeners
-            that.content = content;
+            this.content = content;
             // execute for ContentExp:
             // 1. transformed to: object[]
             // 2. transformed to: HTMLElement[]
@@ -108,76 +98,17 @@ function vdomApi(that: Vdom) {
             // reattach listeners
         },
 
-        disableDefaultRenderer() {
-            that.mixinDisabledDefaultRenderer = that.mixin;
+        disableDefaultRenderer: () => {
+            this.mixinDisabledDefaultRenderer = this.mixin;
         },
-        queryFirstDetached() {
+        queryFirstDetached: () => {
             // Get the first child that was detached by this mixin 
         }
-    }
-}
-
-
-
-
-
-
-
-
-
-
-/*
-
-type BtldCurrent = { node: BtldVdom, root: BtldVdom, mixin: string };
-
-export function BtldVdom2(current: BtldCurrent) {
-
-	let renderedByMixin: string;
-	let children: BtldVdom[];
-	let tag: string;
-    let synchronizeRender: boolean;
-    let dom: HTMLElement;
-
-    // Open questions
-    // 1. 
-
-    function runRenderer(func) {
-        let builder = { tag, children };
-        func(builder);
-        tag = builder.tag;
-        children = [...builder.children];
-        synchronizeRender = true;
-
-        // TODO directly make dom changes
-        // Use replaceChildren  to render into dom
-    }
-
-	this.render = function (func) {
-		if (!renderedByMixin) renderedByMixin = current.mixin;
-		if (func && renderedByMixin === current.mixin) runRenderer(func);
-	};
-
-	const detachedByMixin: {[mixin: string]: boolean} = {};
-    const dataListeners: {order: number, listener: DataListener}[] = [];
-	
-	this.detach = function (detached: boolean = true) {
-		detachedByMixin[current.mixin] = detached;
-    }
-
-    let localState: {[key: string]: unknown};
-
-    this.setState = function (key: string, state: unknown) {
-        (localState = localState || {})[key] = state;
-    };
-
-    this.getDom = function () {
-        if (tag) return [dom || (dom = document.createElement(tag))];
-        return this.children.flatMap(c => c && c.getDom());
-    };
-
-    this.synchronizeDom = function () {
-        
-        
     };
 };
-*/
+
+type DomListener = (...args: any[]) => void;
+type StateListener = (before: unknown, after: unknown) => void;
+type VdomRoot = { vdom: Vdom };
+type ContentExp = string | { path: string[], listener: StateListener };
+type InnerHTML = string;
