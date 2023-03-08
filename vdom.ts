5012749c69e2a1aa62df7beb7eced87ccecaa53b
Florian Rüberg <florian.rueberg@gmail.com>
2023-03-08 16:52:07 +0300

    Test

--- vdom.ts
+++ vdom.ts
@@ -1,3 +1,71 @@
+function parsePath(s: string): VdomPathParsed {
+  return [''];
+}
+
+function parseText(s: string): VdomContent {
+  return {content: []};
+}
+
+function parseTemplate(s: string): Vdom {
+  return new Vdom();
+}
+
+
+
+type VdomStateListener = (after: any, before: any) => void;
+// Every listener is in a path or managed by a mixin
+type VdomPathParsed = (string | {ref: string})[];
+type VdomPath = {path: VdomPathParsed, listener: VdomStateListener};
+
+interface VdomContent {
+  content: Node[] | Vdom,
+  producer?: VdomPath // Path can point to (api) => Node[]
+}
+
+interface VdomAttrPart {
+  content: unknown, // Stringified if multiple
+  producer?: VdomPath // Result mapped via middleware state
+}
+
+class VdomState {
+  dom: HTMLElement;
+  content: VdomContent[] = [];
+  state: {[key: string]: unknown} = {};
+  listeners: {[key: string]: VdomStateListener[]} = {};
+  attrs: {[attr: string]: VdomAttrPart[]} = {};
+
+  // Is this need - or is every change directly mirrored?
+  rerenderContent: boolean = false;
+  
+  // ??? Expose api as state??
+
+  setState = () => {
+    // Update state
+    // Call listeners
+    // Drill down into content Vdom
+  }
+
+  setAttr = (attr: string, value: unknown, {parse = false}) => {
+    if (parse) {} // parse as well?
+
+    // Produce listeners & detach old
+    //  - stringify if multiple
+    //  ? map via middleware states
+    //  - produce content or attr listeners
+
+    // Actually set the attribute
+  }
+
+  setContent = () => {}
+  addListener = () => {}
+}
+
+
+
+
+
+
+
 export interface Definition {
   // Parsed: tag attr string paths
   createVdom(root: VdomRoot, state: { [key: string]: unknown }): Vdom;
@@ -8,6 +76,20 @@ function root() {
   root.vdom.root = root;
   return root.vdom;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export class Vdom {
   tag: string;
@@ -208,7 +290,5 @@ export class Vdom {
 }
 
 type DomListener = (...args: any[]) => void;
-type StateListener = (after: unknown, before: unknown) => void;
 type VdomRoot = { vdom: Vdom };
-type ContentExp = string | { path: string[]; listener: StateListener };
-type InnerHTML = string;
+
