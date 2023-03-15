3156cd05fedea5f116613efb09a51da6cba088a7
Florian Rüberg <florian.rueberg@gmail.com>
2023-03-15 17:13:31 +0300

    First demo test

---
+++ test/specification/test1.js
@@ -0,0 +1,10 @@
+import { assert } from 'chai';
+
+describe('Array', () => {
+  describe('#indexOf()', () => {
+    it('should return -1 when the value is not present', () => {
+      console.log(document, document.createElement('div'));
+      assert.typeOf('foo', 'string');
+    });
+  });
+});
