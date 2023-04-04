930a020c36167d0a4eb13d77faaee842f18c079d
Florian Rüberg <florian.rueberg@gmail.com>
2023-04-04 22:34:27 +0200

    .

--- src/vdom.ts
+++ src/vdom.ts
@@ -1,40 +1,18 @@
-class VComponent {
-  list: VContainer[] = [];
-}
-
-interface State {}
-
-interface VNodeBuilder {
-  build: (this: VNode) => void;
-  parent?: number;
-}
-
-interface VNode {
-  node: Text | Element;
-  parent: Element;
-  container: VContainer;
-}
-
 class VContainer {
-  constructor(private builder: VNodeBuilder[]) {}
-  nodes: VNode[];
-  state: State;
-  clone() {
-    return new VContainer(this.builder);
-  }
-  attach(parent: Node, before?: Node) {
-    if (!this.nodes) {
-      // new VNode()
-      // Attach to parent
-      // Add to nodes array
-      // this.container.nodes[this.parent as number]?.node;
+  roots: (Text | Element)[];
+  hiddenBy = new Set<string>();
+  clone?: () => VContainer;
+  actualize(parent: Node, list: VContainer[]) {
+    if (this.visible()) {
+      let start = list.indexOf(this) + 1;
+      let found = start > 0 && list.slice(start).find(x => x.visible());
+      let after = (found && found.roots && found.roots[0]) || null;
+      (this.roots || []).forEach(r => parent.insertBefore(r, after));
+    } else {
+      this.roots && this.roots.forEach(r => r.remove());
     }
-    this.roots().forEach(r => parent.insertBefore(r.node, before || null));
   }
-  roots() {
-    return (this.nodes || []).filter(x => !x.parent);
-  }
-  detach() {
-    this.roots().forEach(r => r.node.remove());
+  visible() {
+    return !this.hiddenBy.size && this.roots.length;
   }
 }
