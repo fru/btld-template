e77471187052b131762590f8143e78f0f96049c4
Florian Rüberg <florian.rueberg@gmail.com>
2023-04-29 14:48:58 +0200

    .

--- .build/_cli.mjs
+++ .build/_cli.mjs
@@ -2,9 +2,7 @@
 'use strict';
 
 import { Command } from 'commander';
-import { globSync } from 'glob';
-import path from 'node:path';
-import fs from 'node:fs';
+import { resolveInputGlobs, resolveOutDir } from './helper.mjs';
 
 const program = new Command();
 program
@@ -21,29 +19,12 @@ program
   .option('-l, --languages <lang>', 'Code block languages csv', 'typescript')
   .option('-t, --test', 'Include test code sections')
   .action((globs, options) => {
-    const files = resolveInputFiles(globs, options.ignore);
+    const files = resolveInputGlobs(globs, options.ignore);
     let languages = options.languages.split(',');
-    let out = resolveAndEnsureExists(options.out);
+    let out = resolveOutDir(options.out);
     let test = options.test;
 
     console.log(files, languages, out, test);
   });
 
 program.parse();
-
-function resolveAndEnsureExists(out) {
-  out = path.resolve(out);
-  if (!fs.existsSync(out)) fs.mkdirSync(out, { recursive: true });
-  return out;
-}
-
-function resolveInputFiles(globs, ignoreRaw) {
-  const ignore = ignoreRaw.split(' ').map(x => x.trim());
-  const matchBase = true;
-  const nodir = true;
-  const files = globSync(globs, { ignore, matchBase, nodir });
-  if (!files.length) {
-    program.error('No input files found, use a glob like: src/**.md');
-  }
-  return files;
-}
---
+++ .build/helper.mjs
@@ -0,0 +1,23 @@
+import { globSync } from 'glob';
+import path from 'node:path';
+import fs from 'node:fs';
+import commonPathPrefix from 'common-path-prefix';
+
+export function resolveOutDir(out) {
+  out = path.resolve(out);
+  if (!fs.existsSync(out)) fs.mkdirSync(out, { recursive: true });
+  return out;
+}
+
+export function resolveInputGlobs(globs, ignoreRaw) {
+  const ignore = ignoreRaw.split(' ').map(x => x.trim());
+  const matchBase = true;
+  const nodir = true;
+  const files = globSync(globs, { ignore, matchBase, nodir });
+  if (!files.length) {
+    program.error('No input files found, use a glob like: src/**.md');
+  }
+  const prefix = commonPathPrefix(files);
+  const paths = files.map(x => x.substring(prefix.length));
+  return { prefix: path.resolve(prefix), paths };
+}
