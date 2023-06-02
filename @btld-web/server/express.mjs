afb30b4be05e52a28886cd11e9e1b2dc6668ca80
Florian Rüberg <florian.rueberg@gmail.com>
2023-06-02 21:36:40 +0200

    Update express.mjs

--- @btld-web/server/express.mjs
+++ @btld-web/server/express.mjs
@@ -1,10 +1,11 @@
 import express from 'express';
+import { secrets } from 'docker-secret';
 
 const app = express();
 const port = 4000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World!' + secrets.MAIL_APP_PW);
 });
 
 app.listen(port, () => {
