d5b1be43bfeda4d7a92abfef5825d4ad1ba52fac
Florian Rüberg <florian.rueberg@gmail.com>
2023-06-04 22:57:45 +0200

    Update hocuspocus.mjs

--- @btld-web/server/hocuspocus.mjs
+++ @btld-web/server/hocuspocus.mjs
@@ -1,7 +1,22 @@
 import { Hocuspocus } from '@hocuspocus/server';
 
-const server = new Hocuspocus({
-  port: 4001,
+const server = Hocuspocus.configure({
+  async onAuthenticate(data) {
+    const { token } = data;
+
+    // Example test if a user is authenticated with a token passed from the client
+    if (token !== "super-secret-token") {
+      throw new Error("Not authorized!");
+    }
+
+    // You can set contextual data to use it in other hooks
+    return {
+      user: {
+        id: 1234,
+        name: "John",
+      },
+    };
+  },
 });
 
-server.listen();
+server.listen(4001);
