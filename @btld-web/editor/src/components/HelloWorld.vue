290fd31c476860d5917c7024160e4493a48c9de3
Florian Rüberg <florian.rueberg@gmail.com>
2023-06-04 23:05:19 +0200

    Update HelloWorld.vue

--- @btld-web/editor/src/components/HelloWorld.vue
+++ @btld-web/editor/src/components/HelloWorld.vue
@@ -63,7 +63,7 @@ export default {
     this.provider = new HocuspocusProvider({
       name: 'room1',
       document: ydoc,
-      url: 'ws://127.0.0.1:443',
+      url: 'ws://120.4.4.78:443',
     })
 
     this.provider.on('status', event => {
